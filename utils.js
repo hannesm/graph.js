4d55a6f9e9f7a7eb73711381b23a95ec7612399d
Hannes Mehnert <hannes@mehnert.org>
2014-01-29 21:17:54 +0000

    add curry

--- utils.js
+++ utils.js
@@ -4,9 +4,28 @@ Function.prototype.compose  = function (argFunction) {
         return  invokingFunction.call(this, argFunction.apply(this, arguments))
     }
 }
+
+Function.prototype.curry = function () {
+  // wir merken uns f
+  var f = this
+  if (arguments.length < 1) {
+    return f //nothing to curry with - return function
+  }
+  var a = toArray(arguments)
+  return function () {
+    var b = toArray(arguments)
+    return f.apply(this, a.concat(b))
+  }
+}
+
+function toArray (xs) {
+  return Array.prototype.slice.call(xs)
+}
+
 function getProp (property, object) {
     return object[property]
 }
+
 function eq (a, b) {
     return a == b
 }
