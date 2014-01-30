58205d505139b772059370fe77452eac4710e88c
Hannes Mehnert <hannes@mehnert.org>
2014-01-30 13:22:13 +0000

    more tests

--- tests.js
+++ tests.js
@@ -20,6 +20,10 @@ function assert (name, value, fun, args) {
         count = count + 1
 }
 
+function delay (stuff) {
+    return function () { return stuff }
+}
+
 function testUtils () {
     function id (x) { return x }
 
@@ -44,5 +48,51 @@ function testUtils () {
 }
 
 testUtils()
+
+var epsilon = 0.001
+
+function samesame (x, y) {
+    return Math.abs(x - y) < epsilon
+}
+
+function testCoordinates (name, should, are) {
+    assert(name + " x", true, samesame.curry(should[0]), [are[0]])
+    assert(name + " y", true, samesame.curry(should[1]), [are[1]])
+}
+
+function testPolar () {
+    var pp = new PolarPoint()
+    assert("polarpoint complex has correct x", 0, delay(pp.toComplex()[0]))
+    assert("polarpoint complex has correct y", 0, delay(pp.toComplex()[1]))
+    var oneone = pp.follow(toPolar(1, 1))
+    testCoordinates("polarpoint follow worked", [1, 1], oneone.toComplex())
+    var fourfour = oneone.scale(4)
+    testCoordinates("polarpoint scale worked", [4, 4], fourfour.toComplex())
+    var copy = fourfour.copy()
+    fourfour.rho = 0
+    testCoordinates("polarpoint copy worked", [4, 4], copy.toComplex())
+    testCoordinates("polarpoint copy worked", [0, 0], fourfour.toComplex())
+    var cc = pp.follow(toPolar(10, 10)).follow(toPolar(10, 0))
+    testCoordinates("polarpoint multiple follow worked", [20, 10], cc.toComplex())
+    var dist = oneone.distance(copy)
+    assert("distance is correct", true, samesame.curry(Math.sqrt(3 * 3 + 3 * 3)), [dist])
+    var dist2 = copy.distance(oneone)
+    assert("distance is correct", true, samesame.curry(Math.sqrt(3 * 3 + 3 * 3)), [dist2])
+    var dist3 = cc.distance(pp)
+    assert("distance is correct", true, samesame.curry(Math.sqrt(20 * 20 + 10 * 10)), [dist3])
+}
+
+testPolar()
+
+
+function testGraph () {
+    var graph = new Graph()
+    var n10 = new Node(10)
+    graph.insert(n10)
+    assert("graph insert works", 1, delay(graph.nodes.length))
+
+}
+
+testGraph()
 
 console.log("In", count, "assertions I failed to find any defects")
